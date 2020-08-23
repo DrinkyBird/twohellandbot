09d802f4c58af5c49c390588bb728d9e07442a1d
DrinkyBird <sean@csnxs.uk>
2020-08-23 01:26:22 +0100

    Attempt to fix welcome

--- welcome.py
+++ welcome.py
@@ -12,12 +12,20 @@ class WelcomeCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    def announce_join(self, member):
+        chan = self.bot.get_channel(config.WELCOME_CHANNEL)
+        if not chan is None:
+            await chan.send('Welcome aboard the Ark, %s! Thanks for supporting the show.' % (member.mention,))
+
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
         in_before = member_had_role(before)
         in_after = member_had_role(after)
 
         if not in_before and in_after:
-            chan = self.bot.get_channel(config.WELCOME_CHANNEL)
-            if not chan is None:
-                await chan.send('Welcome aboard the Ark, %s! Thanks for supporting the show.' % (after.mention,))
+            self.announce_join(after)
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        if member_had_role(member):
+            self.announce_join(member)
