f4e94dac3842982bc1ae7f328918fe5d5e85731a
DrinkyBird <sean@csnxs.uk>
2020-08-24 23:43:23 +0100

    Add say cmd

---
+++ admin.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+import config
+
+class AdminCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(help="Speaks a thing", usage="<text>", hidden=True)
+    async def say(self, ctx, target, *args):
+        if ctx.author.id in config.ADMINS:
+            if len(args) < 1:
+                await ctx.send('<@%d> Syntax: `%ssay <channel ID> <text>`' % (ctx.author.id, config.COMMAND_PREFIX))
+                return
+
+            text = ' '.join(args)
+
+            try:
+                channel = self.bot.get_channel(int(target))
+                await channel.send(text)
+            except Exception as e:
+                await ctx.send('An error occured (`' + str(e) + '`) - channel doesn\'t exist, is in invalid format, or isn\'t accessible by the bot')
--- main.py
+++ main.py
@@ -6,10 +6,12 @@ import db
 import systems
 import welcome
 import identicon
+import admin
 
 client = commands.Bot(command_prefix=config.COMMAND_PREFIX)
 client.add_cog(quotes.QuotesCog())
 client.add_cog(systems.SystemsCog())
 client.add_cog(identicon.IdenticonCog())
 client.add_cog(welcome.WelcomeCog(client))
+client.add_cog(admin.AdminCog(client))
 client.run(config.BOT_TOKEN)
