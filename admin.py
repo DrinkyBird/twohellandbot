eb57bdba14c59f47a942f75a9ec07bb15a36c0d6
DrinkyBird <sean@csnxs.uk>
2020-08-28 02:53:32 +0100

    Add sql command

--- admin.py
+++ admin.py
@@ -2,6 +2,10 @@ import discord
 from discord.ext import commands
 import config
 import sys
+import db
+from tabulate import tabulate
+import tempfile
+import os
 
 class AdminCog(commands.Cog):
     def __init__(self, bot):
@@ -27,3 +31,59 @@ class AdminCog(commands.Cog):
         if ctx.author.id in config.ADMINS:
             await self.bot.close()
             sys.exit()
+
+    @commands.command(help="Execute an SQLite query", usage="<query>", hidden=True)
+    async def sql(self, ctx, *args):
+        if ctx.author.id in config.ADMINS:
+            if len(args) < 1:
+                await ctx.send('<@%d> Syntax: `%ssay <channel ID> <text>`' % (ctx.author.id, config.COMMAND_PREFIX))
+                return
+
+            text = ' '.join(args)
+
+            cur = db.get_cursor()
+            try:
+                db.execute(cur, text)
+            except Exception as e:
+                await ctx.send('Error: ' + str(e))
+                return
+
+            rows = cur.fetchall()
+
+            if len(rows) < 1:
+                await ctx.send('0 rows returned.')
+                return
+
+            table = []
+            headers = []
+            i = 0
+            for row in rows:
+                d = dict(row)
+
+                trow = []
+                for col in d:
+                    if  i == 0:
+                        headers.append(col)
+                    trow.append(d[col])
+
+                table.append(trow)
+
+                i += 1
+
+            tabulated = tabulate(table, headers, tablefmt="presto", numalign="left", stralign="left")
+            out = '%d row%s returned.\n' % (len(rows), "" if len(rows) == 1 else "s")
+
+            if len(tabulated) < 1800:
+                out += '```\n'
+                out += tabulated
+                out += '```'
+                await ctx.send(out)
+            else:
+                filepath = "sql_" + str(ctx.message.id) + ".txt"
+                with open(filepath, 'w') as f:
+                    f.write(tabulated)
+                    f.flush()
+
+                    await ctx.send(out, file=discord.File(filepath))
+
+                os.remove(filepath)
