979b319ec8fb7d69bc91f7e6de5e5cd58040add2
Sean Baggaley <me@drinkybird.net>
2021-03-23 21:33:59 +0000

    Increase slot chances

--- currency.py
+++ currency.py
@@ -518,7 +518,7 @@ class CurrencyCog(commands.Cog):
         self.slots_remove_emoji(ls, a, 10)
         b = random.choice(ls)
         if a.id == b.id:
-            self.slots_remove_emoji(ls, a, 50)
+            self.slots_remove_emoji(ls, a, 20)
         c = random.choice(ls)
 
         machine = f"<:{a.name}:{a.id}><:{b.name}:{b.id}><:{c.name}:{c.id}>"
